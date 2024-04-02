72e2f98023d5108d1c337e6eabf6e166b8ed1bda
risunyaaa <risunyaaa@yandex.ru>
2024-04-02 22:34:08 +0300

    v 3.0
    add auth form

--- frontend/App.js
+++ frontend/App.js
@@ -1,27 +1,12 @@
+import Auth from './src/components/auth';
 import './src/styles/global.css';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const App = () => {
-    // Состояние для хранения текущего времени
-    const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString());
 
-    // Функция для обновления текущего времени
-    const updateTime = () => {
-        setCurrentTime(new Date().toLocaleTimeString());
-    };
-
-    // Запускаем таймер при монтировании компонента
-    useEffect(() => {
-        const intervalId = setInterval(updateTime, 1000);
-
-        // Функция очистки таймера при размонтировании компонента
-        return () => clearInterval(intervalId);
-    }, []); // Пустой массив зависимостей означает, что useEffect будет вызван только при монтировании и размонтировании компонента
-
-    return (
+    return (    
         <>
-            <h1>Привет, мир!</h1>
-            <h2>Сейчас {currentTime}</h2>
+            <Auth></Auth>
         </>
     );
 };
